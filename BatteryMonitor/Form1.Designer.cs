23f129c59c331dc9c97a0f579f040e5cd58884e2
mariamT.purrahoo@gmail.com <mariamT.purrahoo@gmail.com>
2019-09-20 12:41:00 -0700

    Refresh Button Added

--- BatteryMonitor/Form1.Designer.cs
+++ BatteryMonitor/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.btnCloseWindow = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -83,7 +84,7 @@
             // 
             // btnCloseWindow
             // 
-            this.btnCloseWindow.Location = new System.Drawing.Point(203, 323);
+            this.btnCloseWindow.Location = new System.Drawing.Point(112, 330);
             this.btnCloseWindow.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCloseWindow.Name = "btnCloseWindow";
             this.btnCloseWindow.Size = new System.Drawing.Size(139, 35);
@@ -92,11 +93,21 @@
             this.btnCloseWindow.UseVisualStyleBackColor = true;
             this.btnCloseWindow.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(310, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 35);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.ClientSize = new System.Drawing.Size(541, 461);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnCloseWindow);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -118,6 +129,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnCloseWindow;
+        private System.Windows.Forms.Button button1;
     }
 }
 
