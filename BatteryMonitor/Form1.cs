0f1cfdc8e4e962314e5b6923cedd40bc1c2458e9
mariamT.purrahoo@gmail.com <mariamT.purrahoo@gmail.com>
2019-09-20 11:51:02 -0700

    Added Labels to form

--- BatteryMonitor/Form1.cs
+++ BatteryMonitor/Form1.cs
@@ -16,5 +16,10 @@ namespace BatteryMonitor
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
